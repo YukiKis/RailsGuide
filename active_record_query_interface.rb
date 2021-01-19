619f8cb60ed5bd4eed7c5f4a35eef7b10292e491
Yuki-K <s143068@gmail.com>
2021-01-19 12:31:39 +0900

    finish active_record_query_interface

--- active_record_query_interface.rb
+++ active_record_query_interface.rb
@@ -259,3 +259,204 @@ Client.joins(:orders).where(orders: { created_at: time_range })
 Author.left_outer_joins(:posts).distinct.select("authors.*, COUNT(posts.*) AS posts_count").group("authors.id")
 SELECT DISTINCT authors.*, COUNT(posts.*) AS posts_count FROM "authors"
 LEFT OUTER JOIN posts ON posts.author_id = authors.id GROUP BY authors.id
+
+clients = Client.limit(10)
+clients.each do |client|
+  puts client.address.postcode
+end
+
+clients = Client.includes(:address).limit(10)
+clients.each do |client|
+  puts client.address.postcode
+end
+SELECT * FROM clients LIMIT 10
+SELECT addresses.* FROM addressed 
+WHERE addresses.client_id IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+Article.includes(:category, :comments)
+Category.includes(articles: [{ comments: :guest}, :tags]).find(1)
+
+Article.includes(:comments).where(comments: { visible: true })
+SELECT "articles"."id" AS t0_r0, ... "comments"."updated_at" AS t1_r5
+FROM "articles"
+LEFT OUTER JOIN "comments" ON "comments"."article_id" = "articles"."id"
+WHERE (comments.visible = 1)
+
+class Article < ApplicationRecord
+  scope :published, ->(){ where(published: true)}
+  scope :publisehd_and_commentsd, ->(){ published.where("comments_count > 0")}
+  def self.published
+    where(publied: true)
+  end
+end
+Artcile.published
+category = Category.first
+category.articles.published
+
+class Article < ApplicationRecord
+  scope :created_before, ->(time){ where("created_at < ?", time) }
+end
+Article.created_before(Time.zone.now)
+
+def self.created_before(time)
+  where("created_at < ?", time)
+end
+category.articles.created_before(time)
+
+class Article < ApplicationRecord
+  scope :created_before, ->(time){ where("created_at < ?", time) if time.present? }
+  def self.created_before(time)
+    where("created_at < ?", time) if time.present?
+end
+
+class Client < ApplicationRecord
+  default_sope { where("removed_at IS NULL") }
+end
+SELECT * FROM clients WHERE removed_at IS NULL
+
+def Client < ApplicationRecord
+  default_scope { where(active: true) }
+end
+Client.new
+Client.unscoped.new
+
+class User < ApplicationRecord
+  default_scope { where state: "pending" }
+  scope :active, ->(){ where(state: "active")}
+  scope :inactive, ->(){ where state: "inactive" }
+end
+
+User.active.inactive
+SELECT * FROM users WHERE "state" = "active" AND "state" = "inactive"
+
+User.active.merge(User.inactive)
+SELECT "users".* FROM users WHERE "users"."state" = "inactive"
+
+User.all
+SELECT "users".* FROM users WHERE "users"."state" = "pending"
+User.active
+SELECT "users".* FROM users WHERE "users"."state" = "pending" AND "users"."state" = "active"
+
+Client.unscoped.load
+Client.unscoped.all
+SELECT "clients".* FROM "clients"
+Client.where(published: false).unscoped.all
+SELECT "clients".* FROM "clients"
+Client.unscoped{
+  Client.created_before(Time.zone.now)
+}
+
+class Book < ApplicationRecord
+  enum availability: [:available, :unavailable]
+end
+
+Book.available
+Book.where(availability: :available)
+book = Book.new(availability: :available)
+book.available?
+book.unavailable!
+book.available?
+
+Person
+  .select("people.id, people.name, comments.text")
+  .joins(:comments)
+  .where("comments.created_at > ?", 1.week.ago)
+SELECT "people"."id", "people"."name", "comments"."text"
+  FROM people
+  INNER JOIN comments ON "people"."id" = "comments"."person_id"
+  WHERE "comments"."created_at" > "2015-01-01"
+
+Person
+  .select("people.id, people.name, companies.name")
+  .joins(:company)
+  .find_by("people.name"=> "John")
+SELECT people.id, people.name, companies.name
+  FROM people
+  INNER JOIN companies ON companies.person_id = people.id
+  WHERE people.name = "John"
+  LIMIT 1 
+
+Client.find_or_create_by(first_name: "Andy")
+SELECT * FROM clients WHERE clients.first_name = "Andy" LIMIT 1
+BEGIN
+  INSERT INTO clients (created_at, first_name, locked, orders_count, updated_at) VALUES ("2011-08-30-05:22:56", "andy", 1, NULL, "2011-08-30-05:22:57")
+COMMIT
+
+Client.create_with(locked: false).find_or_create_by(first_name: "Andy")
+Client.find_or_create_by(first_name: "Andy") do |c|
+  c.locked = false
+end
+
+validates :orders_count, presence: true
+Client.find_or_create_by(first_name: "Andy")
+
+nick = Client.find_or_initialize_by(first_name: "Nick")
+nick.persisted?
+nick.new_record?
+SELECT * FROM clients WHERE clients.first_name = "Nick" LIMIT 1
+
+Client.find_by_sql("SELECT * FROM clients INNER JOIN orders ON orders.client_id = clients.id ORDER BY clients.created_at DESC")
+
+Client.connection.select_all("SELECT first_name, created_at FROM clients WHERE id = '1'").to_hash
+
+Client.where(active: true).pluck(:id)
+SELECT id FROM clients WHERE active = 1
+Client.distinct.pluck(:role)
+SELECT DISTINCT role FROM clients
+Client.pluck(:id, :name)
+SELECT clients.id, clients.name FROM clients
+Client.select(:id).map { |c| c.id }
+Client.select(:id).map(&:id)
+Client.pluck(:id)
+Client.select(:id, :name).map { |c| [c.id, c.name ]}
+Client.pluck(:id, :name)
+
+class Client < ApplicationRecord
+  def name
+    "I am #{ super }"
+  end
+end
+
+Client.select(:name).map(&:name)
+Client.pluck(:name)
+Client.limit(1).pluck(:name)
+
+Person.ids
+SELECT people.id FROM people
+
+class Person < ApplicationRecord
+  self.primary_key = "person_id"
+end
+Person.ids
+SElECT people.person_id FROM people
+
+Client.exists?(1)
+Client.exists?(id: [1, 2, 3])
+Client.exists?(name: ["John", "Sergei"])
+Client.where(first_name: "Ryan").exists?
+Client.exists?
+Article.any?
+Article.many?
+Article.recent.any?
+Article.recent.many?
+Article.where(published: true).any?
+Article.where(published: true).many?
+Article.first.categories.any?
+Article.first.categories.many?
+
+Client.count
+SELECT COUNT(*) AS count_all FROM clients
+
+Client.where(first_name: "Ryan").count
+SELECT COUNT(*) AS count_all FROM clients WHERE clients.first_name = "Ryan"
+Client.includes(:orders).where(first_name: "Ryan", orders: { status: "recieved"}).count
+SELECT COUNT(DISTINCT clients.id) AS count_all FROM clients
+LEFT_OUTER_JOIN orders ON orders.client_id = clients.id
+WHERE (clients.first_name = "Ryan" ANd orders.status = "received")
+Client.count(:age)
+Client.average(:orders_count)
+Client.minimum(:age)
+Client.maximum(:age)
+Client.sum(:orders_count)
+
+User.where(id: 1).joins(:articles).explain
