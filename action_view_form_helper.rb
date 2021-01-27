a97db676f643cae61f9987906d56f7bd97e7d32a
Yuki-K <s143068@gmail.com>
2021-01-27 22:02:13 +0900

    start action_view_form_helper

---
+++ action_view_form_helper.rb
@@ -0,0 +1,10 @@
+<%= form_tag do %>
+  FOrm contents
+<% end %>
+
+<form accept-charset="UTF-8" action="/home/index" method="post">
+  <div style="margin: 0; padding: 0;">
+    <input name="utf8" type="hidden" value="a;lsjfd">
+    <input name="authenticity_token" type="hidden" value="sadhais;kfjfi">
+  </div>
+</form>
